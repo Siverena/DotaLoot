a471a6c93cc33c7d893598e3eb97cd77a43d17ce
Siverena <siverena.ab@gmail.com>
2020-08-15 12:52:04 +0300

    Добавляет логотип + вертикальное выравнивание

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
             <nav class="navbar navbar-expand-lg ">
                 <!--                <a class="navbar-brand" href="#">Navbar</a>-->
                 <div class="logo">
-                    <a href="index.php"><img src="img/logo.png" alt=""></a>
+                    <a href="index.php"><img src="img/logo_2020.png" alt=""></a>
                 </div>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
